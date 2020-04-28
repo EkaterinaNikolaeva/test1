3e2d4f88fbc80271c51c214efd85edb42b15388a
EkaterinaNikolaeva <57720667+EkaterinaNikolaeva@users.noreply.github.com>
2020-04-28 12:17:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
-
+    return render_template('base.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
